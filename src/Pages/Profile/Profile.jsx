47154a0e987498f86b1a08c3226af1e1fb17ee7e
Atiqur Rahman <s.atiqurrahman2003@gmail.com>
2023-07-27 01:28:40 +0600

    added edit funtionality

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,18 +1,43 @@
 import { useNavigate } from 'react-router-dom';
 import SectionTitle from '../../components/SectionTitle';
 import useAppliedData from '../../DataHouse/useAppliedData';
-
+import { useForm } from 'react-hook-form';
+import toast, { Toaster } from 'react-hot-toast';
+import { useEffect, useState } from 'react';
 const Profile = () => {
-    const [myCollege, applied, user, updatePer, setUpdatePer] = useAppliedData()
-    const navigate = useNavigate()
-    console.log("MyColl", myCollege);
-    console.log("apply", applied);
-    console.log(updatePer);
+    const [myCollege, applied, user, updatePer, setUpdatePer] = useAppliedData();
+    const navigate = useNavigate();
+    const [up, setUp] = useState(false)
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+
+        fetch('https://collegeconnect-orpin.vercel.app/updateData', {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        }).then(response => response.json()).then(data => {
+            console.log(data);
+            if (data.modifiedCount > 0) {
+                toast.success('Info Updated Successfully');
+            }
+        });
+    };
+
+
+
     return (
         <div className='h-screen relative MyContainer'>
             <SectionTitle className="absolute top-0 left-1/2 -translate-x-1/2 " title={"Profile"} />
             <div className='flex justify-center items-center gap-5'>
-                <button className='btn text-[#ff6f26] border-2 font-semibold border-[#ff6f26] px-4 py-2  rounded-lg hover:bg-[#ff6f26] hover:text-white'>Edit Info</button>
+                <button
+                    onClick={() => {
+                        window.my_modal_5.showModal()
+                        setUp(!up)
+                    }
+                    }
+                    className='btn text-[#ff6f26] border-2 font-semibold border-[#ff6f26] px-4 py-2  rounded-lg hover:bg-[#ff6f26] hover:text-white'>Edit Info</button>
                 <button
                     onClick={() => {
                         setUpdatePer(true)
@@ -23,8 +48,8 @@ const Profile = () => {
 
             <div className='flex flex-wrap gap-5 justify-center items-center'>
                 <div className='w-full mt-10 max-w-xl p-10 border border-[#ff6f26] rounded-2xl '>
-                    <img className='rounded-full border border-[#ff6f26] h-20 w-20 mb-10 mx-auto' src={user.photoURL} />
-                    <h2 className='text-4xl font-bold text-[#ff6f26] '>{user.displayName}</h2>
+                    <img className='rounded-full border border-[#ff6f26] h-20 w-20 mb-10 mx-auto' src={applied?.image || user.photoURL} />
+                    <h2 className='text-4xl font-bold text-[#ff6f26] '>{applied?.name || user.displayName}</h2>
                     <p><b>Email: </b>{user?.email}</p>
                     <p><b>Phone: </b>{applied?.mobile_number}</p>
                     <p><b>Date Of Birth: </b>{applied?.date_of_birth}</p>
@@ -49,7 +74,55 @@ const Profile = () => {
 
 
             </div>
-        </div>
+
+            {/* Modal for update info */}
+            {(up || !up) && <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                {/* Remove the inner <form> element */}
+                <form method="dialog" className="modal-box" onSubmit={handleSubmit(onSubmit)}>
+                    <h3 className="font-bold text-lg">Hello! {user?.displayName}</h3>
+                    <p className="py-4 text-red-500 text-center">Press ESC or Click Outside of Modal to close</p>
+                    <div className="flex flex-col gap-3">
+                        {/* <input type="text" value={id} {...register("applied_id")}
+                            className="opacity-0  "
+
+                        /> */}
+
+                        <input type="url" defaultValue={applied?.image || user?.photoURL} placeholder="Your Photo URL" {...register("image", { required: true })}
+                            className="input input-bordered border-[#ff6f26] w-full "
+                        />
+                        <input type="text" defaultValue={applied?.name || user?.displayName} placeholder="Your Name" {...register("name")}
+                            className="input input-bordered border-[#ff6f26] w-full "
+                        />
+
+                        <input type="date"   {...register("date_of_birth")}
+                            className="input input-bordered border-[#ff6f26] w-full "
+                        />
+
+                        <input type="email" value={user?.email} placeholder="Enter Your Email" {...register("email")}
+                            className="input input-bordered border-[#ff6f26] w-full "
+                        />
+
+                        <input type="tel" placeholder="Enter Your Mobile Number" {...register("mobile_number")}
+                            className="input input-bordered border-[#ff6f26] w-full "
+                        />
+
+
+
+
+
+                        <input className="btn text-[#ff6f26] border-2 font-semibold border-[#ff6f26] px-4 py-2  rounded-lg hover:bg-[#ff6f26] hover:text-white w-40 mx-auto" type="submit" value={"Update"} />
+                    </div>
+
+                </form>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>}
+            <Toaster
+                position="top-center"
+                reverseOrder={false}
+            />
+        </div >
     );
 };
 
